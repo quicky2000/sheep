e77741f675a33a67da6d73f953f1138fcddeb411
Julien Thevenon <julien_thevenon@yahoo.fr>
2013-09-13 18:34:00 +0200

    Correct memory leak

--- src/sheep_situation_analyzer.cpp
+++ src/sheep_situation_analyzer.cpp
@@ -1,4 +1,5 @@
 #include "sheep_situation_analyzer.h"
+#include <inttypes.h>
 
 //-----------------------------------------------------------------------------
 sheep_situation_analyzer::sheep_situation_analyzer
@@ -34,7 +35,7 @@ vector<sheep_transition*> sheep_situation_analyzer::getTransitions
   map<pair<unsigned int, unsigned int>,bool >::const_iterator l_iter_end = l_sheeps.end();
 
   // Store which positionis blocked bqnd who is blocking
-  map<pair<unsigned int, unsigned int>,unsigned char > l_blocked_positions;
+  map<pair<unsigned int, unsigned int>,uint32_t> l_blocked_positions;
   
   // loop which cover all sheeps
   while(l_iter != l_iter_end)
@@ -46,21 +47,25 @@ vector<sheep_transition*> sheep_situation_analyzer::getTransitions
       // Compute reachable positions
       set<pair<unsigned int,unsigned int> > l_reachable_positions;
       getReachablePositions(l_reachable_positions,l_x,l_y,l_sheeps);
-	
+      std::cout << "Determine reachable positions for (" << l_x << "," << l_y << ")" << std::endl ;
+      
+
       // Build corresponding transitions and store reachable positions
       set<pair<unsigned int,unsigned int> >::const_iterator l_iter_reachable = l_reachable_positions.begin();
       set<pair<unsigned int,unsigned int> >::const_iterator l_iter_reachable_end = l_reachable_positions.end();
       while(l_iter_reachable != l_iter_reachable_end)
 	{
-	  map<pair<unsigned int, unsigned int>,unsigned char >::iterator l_blocked_iter = l_blocked_positions.find(*l_iter_reachable);
+	  map<pair<unsigned int, unsigned int>,uint32_t >::iterator l_blocked_iter = l_blocked_positions.find(*l_iter_reachable);
 	  if(l_blocked_positions.end() == l_blocked_iter)
 	    {
-	      l_blocked_positions.insert(map<pair<unsigned int,unsigned int>, unsigned char>::value_type(*l_iter_reachable,l_iter->second+1));
+	      l_blocked_iter = l_blocked_positions.insert(map<pair<unsigned int,unsigned int>,uint32_t>::value_type(*l_iter_reachable,( l_iter->second ? 2 : 1))).first;
 	    }
 	  else
 	    {
-	      l_blocked_iter->second |= l_iter->second+1;
+              l_blocked_iter->second |= ( l_iter->second ? 2 : 1);
 	    }
+          std::cout << "-> (" << l_iter_reachable->first << "," << l_iter_reachable->second << ") = " << (uint32_t) l_blocked_iter->second << std::endl ;
+          
 	  l_result.push_back(new sheep_transition(l_iter->first,*l_iter_reachable));
 	  //COMMENT_OUTPUTcout << "X = " << l_iter_reachable->first << "\tY = " << l_iter_reachable->second << endl;
 	  ++l_iter_reachable;
@@ -71,32 +76,36 @@ vector<sheep_transition*> sheep_situation_analyzer::getTransitions
   // Analysing transitions to remove invalid
   vector<sheep_transition*>::const_iterator l_result_iter = l_result.begin();
   vector<sheep_transition*>::const_iterator l_result_iter_end = l_result.end();
-  vector<sheep_transition*> *l_real_result = new vector<sheep_transition*>();
+  vector<sheep_transition*> l_real_result;
   while(l_result_iter != l_result_iter_end)
     {
       bool l_valid = true;
       //check validity of transition
-      map<pair<unsigned int, unsigned int>,unsigned char >::iterator l_blocked_iter = l_blocked_positions.find((*l_result_iter)->getTarget());
+      map<pair<unsigned int, unsigned int>,uint32_t>::iterator l_blocked_iter = l_blocked_positions.find((*l_result_iter)->getTarget());
       if(l_blocked_positions.end() != l_blocked_iter)
 	{
-	  // Check that the position is blocked by a sheep of the same colour
+	  // Check that the position is not blocked by a sheep of different colour
 	  map<pair<unsigned int, unsigned int>,bool >::const_iterator l_sheep_iter = l_sheeps.find((*l_result_iter)->getSource());
 	  assert(l_sheeps.end() != l_sheep_iter);
-	  l_valid = (l_sheep_iter->second+1 == l_blocked_iter->second);
+	  l_valid = 0 == (( l_sheep_iter->second ? 1 : 2) & l_blocked_iter->second);
 	}
       if(l_valid)
 	{
-	  l_real_result->push_back(*l_result_iter);
+          std::cout << "Valid -> " << (*l_result_iter)->toString() << std::endl ;
+	  l_real_result.push_back(*l_result_iter);
 	}
+      else
+        {
+          delete *l_result_iter;
+        }
       ++l_result_iter;
     }
-  return *l_real_result;
+  return l_real_result;
 }
 
 //-----------------------------------------------------------------------------
 void sheep_situation_analyzer::getReachablePositions(set<pair<unsigned int, unsigned int> > & p_result,unsigned int p_x,unsigned int p_y,const map<pair<unsigned int, unsigned int>,bool > &p_sheeps)
 {
-
   // Determination of relative positions compare to the borders
   int l_x_low = -p_x;
   int l_y_low = -p_y;
